468bd0c0eedf49956d8220839a2c50dfb476999c
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-11-19 18:12:44 +0100

    Add cloudinaryLoader function and useCloudinaryLoader prop to Image component

--- components/Image.tsx
+++ components/Image.tsx
@@ -4,6 +4,7 @@ import NextImage, {
 } from 'next/image';
 
 const workerUrl = process.env.NEXT_PUBLIC_WORKER_URL;
+const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
 
 if (!workerUrl) {
   throw new Error(
@@ -11,8 +12,15 @@ if (!workerUrl) {
   );
 }
 
+if (!cloudName) {
+  throw new Error(
+    'NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME is not defined, did you forget to add it?',
+  );
+}
+
 const normalizeSrc = (src: string) => (src[0] === '/' ? src.slice(1) : src);
 
+// https://cloudinary.com/guides/front-end-development/integrating-cloudinary-with-next-js
 export function cloudinaryLoader({ src, width, quality }: ImageLoaderProps) {
   const params = [
     'f_auto',
@@ -20,15 +28,31 @@ export function cloudinaryLoader({ src, width, quality }: ImageLoaderProps) {
     'w_' + width,
     'q_' + (quality || 'auto'),
   ];
-  return `${workerUrl}/cache/https://res.cloudinary.com/${
-    process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
-  }/image/fetch/${params.join(',')}/${normalizeSrc(src)}`;
+
+  const url = `${workerUrl}/cache/https://res.cloudinary.com/${cloudName}/image/fetch/${params.join(
+    ',',
+  )}/${normalizeSrc(src)}`;
+
+  const isLocalFile = typeof src === 'string' && src.startsWith('/');
+  const hasNoImage = typeof src === 'string' && src === '';
+
+  if (isLocalFile) {
+    return src;
+  }
+
+  if (hasNoImage) {
+    return '@/public/fallback-image.webp';
+  }
+
+  return url;
 }
 
-type ImageProps = Omit<NextImageProps, 'loader'>;
+type ImageProps = Omit<NextImageProps, 'loader'> & {
+  useCloudinaryLoader?: boolean;
+};
 
-const Image = (props: ImageProps) => (
-  <NextImage loader={cloudinaryLoader} {...props} />
-);
+const Image = ({ useCloudinaryLoader = true, ...props }: ImageProps) => {
+  return <NextImage loader={cloudinaryLoader} {...props} />;
+};
 
 export default Image;
