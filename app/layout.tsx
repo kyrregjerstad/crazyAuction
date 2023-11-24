27db89a02a041a427a7572fe7b156b448296a57b
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-11-24 16:51:26 +0100

    add hotjar tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from 'geist/font/sans';
 import AuthProvider from './auth/Provider';
 import './globals.css';
 import QueryClientProvider from './providers/QueryClientProvider';
+import Script from 'next/script';
 
 export default function RootLayout({
   children,
@@ -11,19 +12,31 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      lang='en'
-      className={`${GeistSans.variable} ${GeistMono.variable} dark`}
-    >
-      <body>
-        <QueryClientProvider>
-          <AuthProvider>
-            {/* <Header /> */}
-            {children}
-            <Toaster />
-          </AuthProvider>
-        </QueryClientProvider>
-      </body>
-    </html>
+    <>
+      <Script id='hotjar'>
+        {`(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3751458,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+      </Script>
+      <html
+        lang='en'
+        className={`${GeistSans.variable} ${GeistMono.variable} dark`}
+      >
+        <body>
+          <QueryClientProvider>
+            <AuthProvider>
+              {/* <Header /> */}
+              {children}
+              <Toaster />
+            </AuthProvider>
+          </QueryClientProvider>
+        </body>
+      </html>
+    </>
   );
 }
