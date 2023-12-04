21c2e685f759d9b6a2015a35bad30353546815cb
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-12-04 22:16:13 +0100

    Fix login form validation and error handling

--- components/LoginForm.test.tsx
+++ components/LoginForm.test.tsx
@@ -2,10 +2,9 @@ import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import LoginForm from './LoginForm';
 import { signIn } from 'next-auth/react';
-import { useAnimation } from 'framer-motion';
 import { ReactNode } from 'react';
+import { Mock } from 'vitest';
 
-// Mock dependencies
 vi.mock('next-auth/react', () => ({
   signIn: vi.fn(),
 }));
@@ -15,7 +14,7 @@ vi.mock('framer-motion', () => ({
     start: vi.fn(),
   })),
   motion: {
-    div: ({ children }) => <div>{children}</div>,
+    div: ({ children }: { children: ReactNode }) => <div>{children}</div>,
   },
 }));
 
@@ -26,6 +25,8 @@ vi.mock('next/navigation', () => ({
 }));
 
 describe('LoginForm', () => {
+  const signInMock = signIn as Mock;
+
   it('renders email and password input fields', () => {
     render(<LoginForm />);
 
@@ -42,12 +43,12 @@ describe('LoginForm', () => {
     const passwordInput = screen.getByPlaceholderText('Password');
     const submitButton = screen.getByRole('button', { name: /login/i });
 
-    userEvent.type(emailInput, 'test@stud.noroff.no');
-    userEvent.type(passwordInput, 'password123');
+    await userEvent.type(emailInput, 'test@stud.noroff.no');
+    await userEvent.type(passwordInput, 'password123');
     userEvent.click(submitButton);
 
     await waitFor(() => {
-      expect(signIn).toHaveBeenCalledWith('credentials', {
+      expect(signInMock).toHaveBeenCalledWith('credentials', {
         email: 'test@stud.noroff.no',
         password: 'password123',
         redirect: false,
@@ -55,22 +56,52 @@ describe('LoginForm', () => {
     });
   });
 
-  // it('displays error message when login fails', async () => {
-  //   signIn.mockResolvedValue({ ok: false }); // Simulating a failed login attempt
-  //   render(<LoginForm />);
+  it('displays error message when login fails', async () => {
+    signInMock.mockResolvedValue({ ok: false }); // Simulating a failed login attempt
+    render(<LoginForm />);
 
-  //   const emailInput = screen.getByPlaceholderText('your-email@stud.noroff.no');
-  //   const passwordInput = screen.getByPlaceholderText('Password');
-  //   userEvent.type(emailInput, 'wrong@stud.noroff.no');
-  //   userEvent.type(passwordInput, 'wrongpassword');
-  //   fireEvent.submit(screen.getByRole('button', { name: /login/i }));
+    const emailInput = screen.getByPlaceholderText('your-email@stud.noroff.no');
+    const passwordInput = screen.getByPlaceholderText('Password');
+    await userEvent.type(emailInput, 'wrong@stud.noroff.no');
+    await userEvent.type(passwordInput, 'wrongpassword');
+    fireEvent.submit(screen.getByRole('button', { name: /login/i }));
 
-  //   await waitFor(() => {
-  //     expect(
-  //       screen.getByText('Email and password combination not found'),
-  //     ).toBeInTheDocument();
-  //   });
-  // });
+    await waitFor(() => {
+      expect(
+        screen.getByText('Email and password combination not found'),
+      ).toBeInTheDocument();
+    });
+  });
 
-  // Additional tests can be written to cover other scenarios like animations, successful login, etc.
+  it('displays error message when email is not valid', async () => {
+    render(<LoginForm />);
+
+    const emailInput = screen.getByPlaceholderText('your-email@stud.noroff.no');
+    const passwordInput = screen.getByPlaceholderText('Password');
+    await userEvent.type(emailInput, 'wrong@test.no');
+    await userEvent.type(passwordInput, 'wrongpassword');
+    fireEvent.submit(screen.getByRole('button', { name: /login/i }));
+
+    await waitFor(() => {
+      expect(
+        screen.getByText('Email must end with @stud.noroff.no'),
+      ).toBeInTheDocument();
+    });
+  });
+
+  it('displays error message when password is too short', async () => {
+    render(<LoginForm />);
+
+    const emailInput = screen.getByPlaceholderText('your-email@stud.noroff.no');
+    const passwordInput = screen.getByPlaceholderText('Password');
+    await userEvent.type(emailInput, 'wrong@stud.noroff.no');
+    await userEvent.type(passwordInput, 'test');
+    fireEvent.submit(screen.getByRole('button', { name: /login/i }));
+
+    await waitFor(() => {
+      expect(
+        screen.getByText('Password must be at least 8 characters long'),
+      ).toBeInTheDocument();
+    });
+  });
 });
