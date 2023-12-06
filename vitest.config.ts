274415bc0acbc68708897b81934ed0a15d88cb2c
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-12-06 21:32:46 +0100

    Exclude node_modules directory from test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './test/setup.ts',
-    exclude: ['playwright/**/*'],
+    exclude: ['playwright/**/*', 'node_modules/**/*'],
   },
   resolve: {
     alias: {
