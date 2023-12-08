35e7e84432cad7454c8996ef269c32c31cc28432
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-12-08 15:15:27 +0100

    Add check for CLOUDINARY_API_SECRET in getCloudinarySignature()

--- lib/server/actions.ts
+++ lib/server/actions.ts
@@ -109,9 +109,12 @@ export async function getCloudinarySignature() {
     folder: 'crazy_auction',
   };
 
+  const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+  if (!CLOUDINARY_API_SECRET) throw new Error('No CLOUDINARY_API_SECRET');
+
   const signature = cloudinary.utils.api_sign_request(
     params,
-    process.env.CLOUDINARY_API_SECRET!,
+    CLOUDINARY_API_SECRET,
   );
 
   console.log('timestamp ', timestamp);
