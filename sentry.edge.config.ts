5be365de8eb728fd2e9fb318fdc36f2fa1416aa9
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-12-08 20:16:26 +0100

    Update Sentry configuration

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,10 +3,13 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://2319454fed403bc67cdfe07955e62712@o4505472686817280.ingest.sentry.io/4506280901607424",
+  dsn:
+    process.env.NODE_ENV === 'development'
+      ? undefined
+      : 'https://2319454fed403bc67cdfe07955e62712@o4505472686817280.ingest.sentry.io/4506280901607424',
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
