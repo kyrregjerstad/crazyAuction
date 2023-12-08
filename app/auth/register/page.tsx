d9af17787b5bbce44bc77e3cf9a1f98da2a1ed96
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-12-08 17:15:56 +0100

    update mobile layout

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from '@/components/RegisterForm';
 
 const RegisterPage = () => {
   return (
-    <div className='mx-auto w-full max-w-md space-y-6 pt-8'>
+    <div className='mx-auto w-full max-w-md space-y-4 px-4 pt-8 sm:space-y-6'>
       <RegisterForm />
     </div>
   );
