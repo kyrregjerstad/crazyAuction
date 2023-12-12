fe8dbe843cf6c2eb8092477e90890b0709cdf471
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-12-12 20:42:58 +0100

    Add search button to SearchInput component

--- components/Search.tsx
+++ components/Search.tsx
@@ -5,6 +5,7 @@ import { createUrl } from '@/lib/utils';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { Input } from './ui/input';
 import { FormEvent } from 'react';
+import { Button } from './ui/button';
 
 const SearchInput = () => {
   const router = useRouter();
@@ -29,11 +30,13 @@ const SearchInput = () => {
         name='search'
         placeholder='Search...'
         defaultValue={searchParams?.get('q') || ''}
-        className='w-full rounded-lg border bg-white px-4 py-2 text-sm text-black placeholder:text-neutral-500 dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400'
+        className='w-full rounded-lg border bg-background px-4 py-2 text-sm text-foreground placeholder:text-neutral-500'
         autoComplete='off'
       />
       <div className='absolute right-0 top-0 mr-3 flex h-full items-center'>
-        <SearchIcon />
+        <Button variant='text' className='text-foreground'>
+          <SearchIcon />
+        </Button>
       </div>
     </form>
   );
