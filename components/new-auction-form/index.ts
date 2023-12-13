8318f30e7114c2f4297efaba5f650581ae3fb6ba
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-12-13 01:07:29 +0100

    only keep shields in branch

--- components/new-auction-form/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as NewAuctionForm } from './NewAuctionForm';
--- components/new-auction-form/steps/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as DateStepForm } from './DateStepForm';
-export { default as InfoStepForm } from './InfoStepForm';
-export { default as MediaStepForm } from './MediaStepForm';
-export { default as SummaryStepForm } from './SummaryStepForm';
