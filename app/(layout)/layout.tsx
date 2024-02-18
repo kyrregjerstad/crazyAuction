e2df96b0e2968debe7b9093ecaf70d0cc5bb641d
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2024-02-18 10:47:03 +0100

    Update main layout class to fix header size calculation

--- app/(layout)/layout.tsx
+++ app/(layout)/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
     <>
       <Header />
       {/* 4 rem is the size of the header */}
-      <main className='min-h-[100dvh - 4rem] flex flex-col items-center justify-between'>
+      <main className='flex min-h-[calc(100dvh-4rem)] flex-col items-center justify-between'>
         {children}
       </main>
       <Footer />
